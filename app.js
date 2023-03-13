ed205c187978aa3e9118afa8fd5df6fe75f2a680
Ashin Amanulla <ashin.jnv@gmail.com>
2023-03-13 15:40:15 +0530

    test2

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const app = express()
 require('dotenv').config() // to store secret links,codes etc
 require('./config/db')  // to connect db in server
 const PORT = 2300 || process.env.PORT
-
+const cors = require('cors')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cors())
+app.use(helmet());
+app.use(morgan('dev'))
+app.use(compression())
+
+
 
 
 const art = require('./routes/art')
-
-
 
 
 app.use('/art',art)
